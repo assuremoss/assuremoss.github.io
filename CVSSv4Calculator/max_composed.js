12ed2d3ceadf7b1c14021f3812536c0c16493aa5
gditizio <giorgio.ditizio@unitn.it>
2023-04-03 13:28:28 +0200

    Added missing max in EQ3&EQ6

--- CVSSv4Calculator/max_composed.js
+++ CVSSv4Calculator/max_composed.js
@@ -16,7 +16,7 @@ maxComposed = {
 	},
 	"eq3" : {
 		"0" : {"0": ["VC:H/VI:H/VA:H/CR:H/IR:H/AR:H/"], "1": ["VC:H/VI:H/VA:L/CR:M/IR:M/AR:H/","VC:H/VI:H/VA:H/CR:M/IR:M/AR:M/"]},
-		"1" : {"0": ["VC:L/VI:H/VA:H/CR:H/IR:H/AR:H/","VC:H/VI:L/VA:H/CR:H/IR:H/AR:H/"], "1": ["VC:L/VI:H/VA:L/CR:H/IR:M/AR:H/","VC:L/VI:H/VA:H/CR:H/IR:M/AR:M/","VC:H/VI:L/VA:H/CR:M/IR:H/AR:M/","VC:H/VI:L/VA:L/CR:M/IR:H/AR:H/"]},
+		"1" : {"0": ["VC:L/VI:H/VA:H/CR:H/IR:H/AR:H/","VC:H/VI:L/VA:H/CR:H/IR:H/AR:H/"], "1": ["VC:L/VI:H/VA:L/CR:H/IR:M/AR:H/","VC:L/VI:H/VA:H/CR:H/IR:M/AR:M/","VC:H/VI:L/VA:H/CR:M/IR:H/AR:M/","VC:H/VI:L/VA:L/CR:M/IR:H/AR:H/","VC:L/VI:L/VA:H/CR:H/IR:H/AR:M/"]},
 		"2" : {"1": ["VC:L/VI:L/VA:L/CR:H/IR:H/AR:H/"]},
 	},
 	"eq4" : {
