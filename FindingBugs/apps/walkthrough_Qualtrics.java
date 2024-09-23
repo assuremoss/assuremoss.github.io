5c88a8d0b1e7b3fd9e56a6950539adbcf954d984
Aurora Papotti <99885453+yurrotz@users.noreply.github.com>
2024-09-23 16:27:06 +0200

    Add files via upload

---
+++ FindingBugs/apps/walkthrough_Qualtrics.java
@@ -0,0 +1,161 @@
+package testcases.s12;
+
+import testcasesupport.*;
+import javax.servlet.http.*;
+import java.sql.*;
+import java.util.logging.Level;
+
+public class TestCase_12 extends AbstractTestCase
+{
+    private void query_tmp(String data ) throws Throwable
+    {
+
+        if (data != null)
+        {
+            String names[] = data.split("-");
+            int successCount = 0;
+            Connection dbConnection = null;
+            Statement sqlStatement = null;
+            try
+            {
+                dbConnection = IO.getDBConnection();
+                sqlStatement = dbConnection.createStatement();
+                for (int i = 0; i < names.length; i++)
+                {
+                    sqlStatement.addBatch("update users set hitcount=hitcount+1 where name='" + names[i] + "'");
+                }
+                int resultsArray[] = sqlStatement.executeBatch();
+                for (int i = 0; i < names.length; i++)
+                {
+                    if (resultsArray[i] > 0)
+                    {
+                        successCount++;
+                    }
+                }
+                IO.writeLine("Succeeded in " + successCount + " out of " + names.length + " queries.");
+            }
+            catch (SQLException exceptSql)
+            {
+                IO.logger.log(Level.WARNING, "Error getting database connection", exceptSql);
+            }
+            finally
+            {
+                try
+                {
+                    if (sqlStatement != null)
+                    {
+                        sqlStatement.close();
+                    }
+                }
+                catch (SQLException exceptSql)
+                {
+                    IO.logger.log(Level.WARNING, "Error closing Statament", exceptSql);
+                }
+
+                try
+                {
+                    if (dbConnection != null)
+                    {
+                        dbConnection.close();
+                    }
+                }
+                catch (SQLException exceptSql)
+                {
+                    IO.logger.log(Level.WARNING, "Error closing Connection", exceptSql);
+                }
+            }
+        }
+
+    }
+
+    public void tmp() throws Throwable
+    {
+        String data;
+
+        data = System.getProperty("user.tmp");
+
+        query_tmp(data  );
+    }
+
+    private void query_home(String data) throws Throwable
+    {
+
+        if (data != null)
+        {
+            String names[] = data.split("-");
+            int successCount = 0;
+            Connection dbConnection = null;
+            PreparedStatement sqlStatement = null;
+            try
+            {
+                dbConnection = IO.getDBConnection();
+                sqlStatement = dbConnection.prepareStatement("update users set hitcount=hitcount+1 where name=?");
+                for (int i = 0; i < names.length; i++)
+                {
+                    sqlStatement.setString(1, names[i]);
+                    sqlStatement.addBatch();
+                }
+                int resultsArray[] = sqlStatement.executeBatch();
+                for (int i = 0; i < names.length; i++)
+                {
+                    if (resultsArray[i] > 0)
+                    {
+                        successCount++;
+                    }
+                }
+                IO.writeLine("Succeeded in " + successCount + " out of " + names.length + " queries.");
+            }
+            catch (SQLException exceptSql)
+            {
+                IO.logger.log(Level.WARNING, "Error getting database connection", exceptSql);
+            }
+            finally
+            {
+                try
+                {
+                    if (sqlStatement != null)
+                    {
+                        sqlStatement.close();
+                    }
+                }
+                catch (SQLException exceptSql)
+                {
+                    IO.logger.log(Level.WARNING, "Error closing PreparedStatement", exceptSql);
+                }
+
+                try
+                {
+                    if (dbConnection != null)
+                    {
+                        dbConnection.close();
+                    }
+                }
+                catch (SQLException exceptSql)
+                {
+                    IO.logger.log(Level.WARNING, "Error closing Connection", exceptSql);
+                }
+            }
+        }
+
+    }
+
+    private void home() throws Throwable
+    {
+        String data;
+
+        data = System.getProperty("user.home");
+
+        query_home(data);
+    }
+
+    /* Below is the main(). It is only used when building this testcase on
+     * its own for testing or for building a binary to use in testing binary
+     * analysis tools. It is not used when compiling all the testcases as one
+     * application, which is how source code analysis tools are tested.
+     */
+    public static void main(String[] args) throws ClassNotFoundException,
+           InstantiationException, IllegalAccessException
+    {
+        mainFromParent(args);
+    }
+}
